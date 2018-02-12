e27d78d1a6ce155391a095f74a4d763ecde511bb
Бахтияр <bakhityar_s@mail.ru>
2018-02-13 00:22:15 +0600

    Make app bootable; Add Entity

---
+++ src/main/java/com/sanscrit/task/Task.java
@@ -0,0 +1,43 @@
+package com.sanscrit.task;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private LocalDate date;
+    private boolean status;
+
+    public Task() {};
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
